c86867cdf82bd2fe2692cb0504f8febc8fedcd17
anang354 <anangegga@gmail.com>
2026-01-23 11:57:55 +0700

    Ubah tipe data telepon_sekolah di tabel pengaturans

---
+++ database/migrations/2026_01_23_042745_change_telepon_sekolah_type_in_pengaturans_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pengaturans', function (Blueprint $table) {
+            $table->string('telepon_sekolah', 20)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('pengaturans', function (Blueprint $table) {
+            $table->integer('telepon_sekolah')->change();
+        });
+    }
+};
