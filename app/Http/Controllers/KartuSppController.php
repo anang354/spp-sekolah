97883cc0d2dc9139249899247dcf90e3b7a0cc37
anang354 <anangegga@gmail.com>
2026-01-23 15:59:40 +0700

    Rubah logo sesuai jenjang di slip spp

--- app/Http/Controllers/KartuSppController.php
+++ app/Http/Controllers/KartuSppController.php
@@ -12,7 +12,7 @@ class KartuSppController extends Controller
     public function index(Request $request)
     {
         $siswa = \App\Models\Siswa::where('id',$request->id)->with(['tagihans', 'pembayaran', 'kelas', 'alamatSambung'])->first()->toArray();
-        $path = public_path().'/images/logo-sekolah.jpg';
+        $path = public_path().'/images/logo-'.$siswa['kelas']['jenjang'].'.jpg';
         $type = pathinfo($path, PATHINFO_EXTENSION);
         $data = file_get_contents($path);
         $image = 'data:image/'.$type.';base64,'.base64_encode($data);
