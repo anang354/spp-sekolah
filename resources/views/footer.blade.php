3bfede4fda2dccfe45a00786a6a73796b7665d00
anang354 <anangegga@gmail.com>
2026-01-24 08:07:56 +0700

    Perbaikan posisi footer web

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,7 @@
 <div>
-    <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+    <footer class="bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
         <span class="text-xs text-gray-500 sm:text-center dark:text-gray-400">© {{ date('Y') }} Developed by
-            <a href="https://anangegga.my.id" target="_blank" class="text-red-600 hover:underline">Anang Egga</a>. Made with ❤️ and a cup of ☕
+            <a href="https://anangegga.my.id" target="_blank" class="text-red-600 hover:underline">Anang Egga</a>.
         </span>
     </footer>
 </div>
